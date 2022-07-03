3272fc00989437c114cac6aa31aaf09a812e4f4d
Garvit Sharma <61928763+gtxPrime@users.noreply.github.com>
2022-07-03 23:16:59 +0530

    Update welcome.py

--- BullyRobot/modules/welcome.py
+++ BullyRobot/modules/welcome.py
@@ -184,7 +184,7 @@ def new_member(update: Update, context: CallbackContext):
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
                 update.effective_message.reply_text(
-                    "Hey everyone step aside! My owner @gtxPrime just joined the chat 🎉🎉", reply_to_message_id=reply
+                    "Hey everyone step aside ! My owner @gtxPrime just joined the chat !", reply_to_message_id=reply
                 )
                 welcome_log = (
                     f"{html.escape(chat.title)}\n"
